97f09f7f554353a3feb964dbdafadce9620382a2
cho45 <cho45@lowreal.net>
2012-02-28 19:58:59 +0900

    update

--- tass.js
+++ tass.js
@@ -2,48 +2,38 @@
  * @license copyright (c) 2012 cho45 ( www.lowreal.net ) 
  */
 function TASS (c) {
-	var mixins = {};
+	var mixins = TASS.mixins;
+
+	function processInclude (c) {
+		return c.replace(/@include \s*([\w\-]+)(\(.*?\))?\s*;/g, function (_, name, args) {
+			var mixin = mixins[name];
+			if (!mixin) return '/* unknown mixin: ' + name + ' */';
+			var content = mixin.content;
+			if (args) {
+				var scan = args.slice(1, -1);
+				var r = /\s*((?:'(?:[^']|\\')*'|"(?:[^"]|\\")*"|\([^\)]*\)|[^,()])+)|,|./g;
+				var a = [], m; while ((m = r.exec(scan))) if (m[1]) a.push(m[1]);
+				for (var i = 0, len = mixin.args.length; i < len; i++) content = mixin.args[i] + " : " + a[i] + ";\n" + content;
+			}
+			return content;
+		});
+	}
+
 	c = c.replace(/^@mixin\s+([\w\-]+)\s*(\([^\)]+\))?\s*\{([\s\S]*?)^\}/gm, function (_, name, args, content) {
 		if (args) args = args.slice(1, -1).split(/\s*,\s*/);
 		mixins[name] = {
 			args : args,
-			content : content
+			content : processInclude(content)
 		};
 		return '';
 	});
 
-	c = c.replace(/@include \s*([\w\-]+)(\(.*?\))?\s*;/g, function (_, name, args) {
-		var mixin = mixins[name];
-		if (!mixin) return '/* unknown mixin: ' + name + '*/';
-		var content = mixin.content;
-		if (args) {
-			args = args.slice(1, -1).split(/\s*,\s*/);
-			for (var i = 0, len = args.length, inp = false; i < len; i++) {
-				if (~args[i].indexOf('(')) {
-					inp = true;
-				} else
-				if (inp) {
-					if (~args[i].indexOf(')')) inp = false;
-					args[ i - 1 ] += ', ' + args[i];
-					args.splice(i, 1);
-					len--;
-					i--;
-				}
-			}
-			for (var i = 0, len = mixin.args.length; i < len; i++) {
-				// prepend variable expression
-				content = mixin.args[i] + " : " + args[i] + ";\n" + content;
-			}
-		}
-		return content;
-	});
-
 	var nesting  = [ [] ];
 	var level    = [];
-	var variables = {};
-	c = c.replace(/(^[^\{\};]+\{)|(\})|^\s*(\$[\w\-]+\s*:.+);|(\$[\w\-]+)/gm, function (_, open, close, vardef, varget) {
+	var variables = TASS.variables;
+	c = processInclude(c).replace(/(^[^\{\};]+\{)|(\})|^\s*(\$[\w\-]+\s*:.+);|(\$[\w\-]+)/gm, function (_, open, close, vardef, varget) {
 		if (open) {
-			var scope = function (parent) { this.parent = parent };
+			var scope = function (_parent) { this._parent = _parent };
 			scope.prototype = variables;
 			variables = new scope(variables);
 
@@ -54,23 +44,26 @@ function TASS (c) {
 					nextnesting.push( nesting[i].concat(selectors[j]) );
 			nesting = nextnesting;
 			level.push(selectors);
-			return (nesting[0].length - 1 ? "}\n" : "") + nesting.map(function (_) { return _.join(" ") }).join(",\n") + " {";
+
+			var expanded = [];
+			for (var i = 0, len = nesting.length; i < len; i++) expanded.push(nesting[i].join(" "));
+
+			return (nesting[0].length - 1 ? "}\n" : "") + expanded.join(",\n") + " {";
 		} else
 		if (close) {
-			variables = variables.parent;
+			variables = variables._parent;
 
-			var poped = level.pop();
-			nesting.length = nesting.length / poped.length;
+			nesting.length = nesting.length / level.pop().length;
 			for (var i = 0, len = nesting.length; i < len; i++) nesting[i].pop();
 			return (nesting[0].length ? "" : "}");
 		} else
 		if (vardef) {
-			vardef = vardef.split(/\s*:\s*/);
-			variables[vardef[0]] = vardef[1];
+			vardef = vardef.match(/^(\$[\w\-]+)?\s*:\s*(.*)$/);
+			variables[vardef[1]] = vardef[2];
 			return '';
 		} else
 		if (varget) {
-			return variables[varget];
+			return typeof variables[varget] === 'undefined' ?  '/* unknown variable: ' + verget + ' */': variables[varget];
 		}
 	});
 
@@ -78,14 +71,19 @@ function TASS (c) {
 
 	return c;
 }
+TASS.version = '0.1.1';
+TASS.init = function () {
+	TASS.mixins = {};
+	TASS.variables = {};
+};
+TASS.init();
 
 this.TASS = TASS;
-this.TASS.version = '0.1.0';
 
-if (typeof document != 'undefined') {
+if (typeof document != 'undefined') (function () {
 	var links = document.getElementsByTagName('link');
-	for (var i = 0, it; (it = links[i]); i++) {
-		if (it.rel != 'stylesheet/tass' || !it.href) continue;
+	for (var i = 0, it; (it = links[i]); i++) (function (link) {
+		if (link.rel != 'stylesheet/tass' || !link.href) return;
 		var req;
 		try {
 			req = new XMLHttpRequest();
@@ -101,7 +99,7 @@ if (typeof document != 'undefined') {
 			}
 		}
 
-		req.open('GET', it.href, true);
+		req.open('GET', link.href, true);
 		req.onreadystatechange = function (e) {
 			if (req.readyState != 4) return;
 			if (req.status == 200) {
@@ -109,11 +107,11 @@ if (typeof document != 'undefined') {
 				var style = document.createElement('style');
 				style.type = 'text/css';
 				style.appendChild(document.createTextNode(css));
-				document.getElementsByTagName('head')[0].appendChild(style);
+				link.parentNode.insertBefore(style, link);
 			} else {
 				console.log('[TASS] Error:' + req.status + ' ' + req.responseText);
 			}
 		};
 		req.send(null);
-	}
-}
+	})(it);
+})();
